6b55446f57e2bc69c8398363662af98dba5c974d
rosslogan702 <rosslogan702@gmail.com>
2018-01-16 20:18:32 +0000

    Initial commit

---
+++ src/main/java/com/rosslogan/countries/AppConfig.java
@@ -0,0 +1,14 @@
+package com.rosslogan.countries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+
+@EnableAutoConfiguration
+public class AppConfig {
+    public static void main(String[] args) {
+        SpringApplication.run(AppConfig.class, args);
+
+    }
+
+}
